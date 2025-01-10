fd2599316c3b39067b5bdc8d20df79d4f0cc48e6
Diggo Silva <rdiggosilva@gmail.com>
2025-01-10 08:55:54 -0300

    Configured design

--- CryptoCoin/App/Scenes/CoinsViewController.swift
+++ CryptoCoin/App/Scenes/CoinsViewController.swift
@@ -9,15 +9,50 @@ import UIKit
 
 class CoinsViewController: UIViewController {
     
-    let service = Service()
+    let coinView = CoinView()
+    
+    override func loadView() {
+        super.loadView()
+        view = coinView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        service.getCoins { coins in
-            print("COINS AQUI: \(coins)")
-        } onError: { error in
-            
-        }
+        setNavBar()
+        setDelegatesAndDataSources()
+    }
+    
+    private func setNavBar() {
+        title = "Preços Online"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func setDelegatesAndDataSources() {
+        coinView.collectionView.delegate = self
+        coinView.collectionView.dataSource = self
+        coinView.tableview.delegate = self
+        coinView.tableview.dataSource = self
+    }
+}
+
+extension CoinsViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionCell.identifier, for: indexPath) as? CollectionCell else { return UICollectionViewCell() }
+        return cell
+    }
+}
+
+extension CoinsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TableCell.identifier, for: indexPath) as? TableCell else { return UITableViewCell() }
+        return cell
     }
 }
