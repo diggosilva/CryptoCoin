fb1ce859c3a9c6ec4a4c7c26fa0d995c1598e34d
Diggo Silva <rdiggosilva@gmail.com>
2025-01-11 08:42:02 -0300

    Implementa serviço de requisição para obter dados de moedas do Brasil
    - Adiciona a função getCoinsBR para comunicação com a API
    - Inclui tratamento de sucesso e erro utilizando callbacks

--- CryptoCoin/App/Network/Service.swift
+++ CryptoCoin/App/Network/Service.swift
@@ -9,6 +9,7 @@ import Foundation
 
 protocol ServiceProtocol {
     func getCoins(onSuccess: @escaping([CoinResponse]) -> Void, onError: @escaping(Error) -> Void)
+    func getCoinsBR(onSuccess: @escaping ([CoinResponse]) -> Void, onError: @escaping (any Error) -> Void)
 }
 
 final class Service: ServiceProtocol {
@@ -17,7 +18,27 @@ final class Service: ServiceProtocol {
     var apiUrl = APIClient.getURL()
     
     func getCoins(onSuccess: @escaping ([CoinResponse]) -> Void, onError: @escaping (any Error) -> Void) {
-        guard let url = URL(string: apiUrl) else { return }
+        guard let url = URL(string: APIClient.apiUSA) else { return }
+        
+        dataTask = URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
+            DispatchQueue.main.async {
+                if let response = response as? HTTPURLResponse {
+                    print("DEBUG: StatusCode.. \(response.statusCode)")
+                }
+                do {
+                    let response = try JSONDecoder().decode([CoinResponse].self, from: data ?? Data())
+                    onSuccess(response)
+                } catch {
+                    onError(error)
+                    print("DEBUG: Erro ao decodificar CoinResponse.. \(error.localizedDescription)")
+                }
+            }
+        })
+        dataTask?.resume()
+    }
+    
+    func getCoinsBR(onSuccess: @escaping ([CoinResponse]) -> Void, onError: @escaping (any Error) -> Void) {
+        guard let url = URL(string: APIClient.apiBRA) else { return }
         
         dataTask = URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
             DispatchQueue.main.async {
