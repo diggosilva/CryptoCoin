16bee3f91a44a1b5a5b00e8f19faa9d2fccd8c30
Diggo Silva <rdiggosilva@gmail.com>
2025-01-11 08:34:14 -0300

    Created static let for APIs

--- CryptoCoin/App/Network/APIClient.swift
+++ CryptoCoin/App/Network/APIClient.swift
@@ -7,6 +7,9 @@
 import Foundation
 
 class APIClient {
+    static let apiUSA = Bundle.main.object(forInfoDictionaryKey: "ApiUrlUS") as! String
+    static let apiBRA = Bundle.main.object(forInfoDictionaryKey: "ApiUrlBR") as! String
+    
     static func getURL() -> String {
         let language = Locale.current.language.languageCode ?? "en"
         
---
+++ CryptoCoin/App/Scenes/CoinsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CoinsViewModel.swift
+//  CryptoCoin
+//
+//  Created by Diggo Silva on 10/01/25.
+//
+
+import Foundation
