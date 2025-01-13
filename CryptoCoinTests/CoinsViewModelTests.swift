a603d9eb91e21660ba719217175db12fa97ada32
Diggo Silva <rdiggosilva@gmail.com>
2025-01-13 10:15:07 -0300

    Created file for start tests

---
+++ CryptoCoinTests/CoinsViewModelTests.swift
@@ -0,0 +1,20 @@
+//
+//  CoinsViewModelTests.swift
+//  CoinsViewModelTests
+//
+//  Created by Diggo Silva on 13/01/25.
+//
+
+import XCTest
+@testable import CryptoCoin
+
+final class CryptoCoinTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+}
