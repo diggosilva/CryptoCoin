d9d822dae53fd39442a2249b0c7c58ff286c68f8
Diggo Silva <rdiggosilva@gmail.com>
2025-06-01 14:13:47 -0300

    Refactored Service file with async await

--- CryptoCoin/App/Network/Service.swift
+++ CryptoCoin/App/Network/Service.swift
@@ -8,48 +8,42 @@
 import Foundation
 
 protocol ServiceProtocol {
-    func getCoins(from url: ApiEnvironment, onSuccess: @escaping([CoinModel]) -> Void, onError: @escaping(Error) -> Void)
+    func getCoins(from url: ApiEnvironment) async throws -> [CoinModel]
 }
 
 final class Service: ServiceProtocol {
-    private var dataTask: URLSessionDataTask?
-    
-    func getCoins(from url: ApiEnvironment, onSuccess: @escaping([CoinModel]) -> Void, onError: @escaping(Error) -> Void) {
+    func getCoins(from url: ApiEnvironment) async throws -> [CoinModel] {
         let apiUrlString: String? = switch url {
         case .apiBRA: Bundle.main.object(forInfoDictionaryKey: "ApiUrlBR") as? String
         case .apiUSA: Bundle.main.object(forInfoDictionaryKey: "ApiUrlUS") as? String
         }
         
         guard let url = URL(string: apiUrlString ?? "") else {
-            onError(NSError(domain: "ServiceError", code: 0, userInfo: [NSLocalizedDescriptionKey: "URL inválida."]))
-            return
+            throw NSError(domain: "ServiceError", code: 0, userInfo: [NSLocalizedDescriptionKey: "URL inválida."])
         }
         
-        dataTask = URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
-            DispatchQueue.main.async {
-                if let response = response as? HTTPURLResponse {
-                    print("DEBUG: StatusCode.. \(response.statusCode)")
-                }
-                do {
-                    let coinResponse = try JSONDecoder().decode([CoinResponse].self, from: data ?? Data())
-                    var coinModels: [CoinModel] = []
-                    
-                    for coinModel in coinResponse {
-                        let coin = CoinModel(marketCapRank: coinModel.marketCapRank,
-                                             image: coinModel.image,
-                                             name: coinModel.name,
-                                             symbol: coinModel.symbol,
-                                             currentPrice: coinModel.currentPrice,
-                                             priceChangePercentage24H: coinModel.priceChangePercentage24H)
-                        coinModels.append(coin)
-                    }
-                    onSuccess(coinModels)
-                } catch {
-                    onError(error)
-                    print("DEBUG: Erro ao decodificar CoinResponse.. \(error.localizedDescription)")
-                }
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        if let response = response as? HTTPURLResponse {
+            print("DEBUG: StatusCode.. \(response.statusCode)")
+        }
+        do {
+            let coinResponse = try JSONDecoder().decode([CoinResponse].self, from: data)
+            var coinModels: [CoinModel] = []
+            
+            for coinModel in coinResponse {
+                let coin = CoinModel(marketCapRank: coinModel.marketCapRank,
+                                     image: coinModel.image,
+                                     name: coinModel.name,
+                                     symbol: coinModel.symbol,
+                                     currentPrice: coinModel.currentPrice,
+                                     priceChangePercentage24H: coinModel.priceChangePercentage24H)
+                coinModels.append(coin)
             }
-        })
-        dataTask?.resume()
+            return coinModels
+        } catch {
+            print("DEBUG: Erro ao decodificar CoinResponse.. \(error.localizedDescription)")
+            throw error
+        }
     }
 }
